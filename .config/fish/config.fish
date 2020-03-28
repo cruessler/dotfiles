81a610b0784c5a6a51c66e4d06cadffe0ea20a8d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-03-28 18:05:49 +0100

    Use fisher as package manager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,3 +28,15 @@ source $HOME/.asdf/asdf.fish
 if test -f $HOME/.config/broot/launcher/fish/br
   source $HOME/.config/broot/launcher/fish/br
 end
+
+# this automatically installs fisher which is why both [fisher] and
+# [completions] are ignored by git
+#
+# [fisher]: .config/fish/functions/fisher.fish
+# [completions]: .config/fish/completions/fisher.fish
+# https://github.com/jorgebucaran/fisher#bootstrap-installation
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end

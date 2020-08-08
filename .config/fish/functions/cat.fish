c5eb36a78ea1f2e4b3a013a892d6393df14b1855
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-08-08 17:37:13 +0200

    Make batcat available as bat

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,3 +1,13 @@
+# the Debian package has the executable as `batcat`
+#
+# mkdir -p ~/.local/bin
+# ln -s /usr/bin/batcat ~/.local/bin/bat
+#
+# https://github.com/sharkdp/bat#on-ubuntu-using-apt
+if type -q batcat; and test ! -e $HOME/.local/bin/bat
+  ln -s /usr/bin/batcat $HOME/.local/bin/bat
+end
+
 if type -q bat
   function cat --wraps=bat --description 'alias cat=bat'
     bat $argv;

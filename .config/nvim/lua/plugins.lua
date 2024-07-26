bb6950fc348a5c0b9ebb5ba28a1e7fc557d61e13
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-07-26 09:27:37 +0200

    Add diff to always installed parsers [nvim]

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -223,7 +223,7 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+      ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "diff" },
       auto_install = true,
       highlight = {
         enable = true,

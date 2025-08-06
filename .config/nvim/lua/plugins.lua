608c901999b01c69c239cc4ffe540ca35ffae4e2
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-08-06 06:49:46 +0200

    Simplify plugin setup [nvim]

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -143,9 +143,7 @@ require("lazy").setup({
   {
     "vuki656/package-info.nvim",
     dependencies = "MunifTanjim/nui.nvim",
-    config = function()
-      require("package-info").setup()
-    end,
+    opts = {},
   },
   "wincent/terminus",
 
